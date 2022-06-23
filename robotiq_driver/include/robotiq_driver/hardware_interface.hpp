b38e761fd5318a7255a5ae15bc87224bffa0f110
Cory Crean <cory.crean@picknik.ai>
2022-06-23 18:39:53 +0000

    Add RobotiqGripperHardwareInterface.

---
+++ robotiq_driver/include/robotiq_driver/hardware_interface.hpp
@@ -0,0 +1,62 @@
+// Copyright 2022 PickNik Inc.
+// All rights reserved.
+//
+// Unauthorized copying of this code base via any medium is strictly prohibited.
+// Proprietary and confidential.
+
+#pragma once
+
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "hardware_interface/actuator_interface.hpp"
+#include "hardware_interface/handle.hpp"
+#include "hardware_interface/hardware_info.hpp"
+#include "hardware_interface/system_interface.hpp"
+#include "hardware_interface/types/hardware_interface_return_values.hpp"
+#include "rclcpp/macros.hpp"
+#include "rclcpp/rclcpp.hpp"
+
+#include "robotiq_driver/visibility_control.h"
+
+namespace ros2_robotiq_driver
+{
+class RobotiqGripperHardwareInterface : public hardware_interface::ActuatorInterface
+{
+public:
+  RCLCPP_SHARED_PTR_DEFINITIONS(RobotiqGripperHardwareInterface)
+
+  ROBOTIQ_DRIVER_PUBLIC
+  CallbackReturn on_init(const hardware_interface::HardwareInfo& info) override;
+
+  ROBOTIQ_DRIVER_PUBLIC
+  std::vector<hardware_interface::StateInterface> export_state_interfaces() override;
+
+  ROBOTIQ_DRIVER_PUBLIC
+  std::vector<hardware_interface::CommandInterface> export_command_interfaces() override;
+
+  ROBOTIQ_DRIVER_PUBLIC
+  CallbackReturn on_activate(const rclcpp_lifecycle::State& previous_state) override;
+
+  ROBOTIQ_DRIVER_PUBLIC
+  CallbackReturn on_deactivate(const rclcpp_lifecycle::State& previous_state) override;
+
+  ROBOTIQ_DRIVER_PUBLIC
+  hardware_interface::return_type read() override;
+
+  ROBOTIQ_DRIVER_PUBLIC
+  hardware_interface::return_type write() override;
+
+private:
+  // Parameters for the RRBot simulation
+  double hw_start_sec_;
+  double hw_stop_sec_;
+  double hw_slowdown_;
+
+  // Store the command for the simulated robot
+  double hw_joint_command_;
+  double hw_joint_state_;
+};
+
+}  // namespace ros2_robotiq_driver
